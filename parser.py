d4ac10ae7a7b720a88e94c4f663a3837063e412b
Lukas Wanko <lwanko.mmt-m2014@fh-salzburg.ac.at>
2015-11-18 10:35:39 +0100

    first commit

---
+++ parser.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import psycopg2
+import sys
+
+con = None
+
+try:
+
+    con = psycopg2.connect(database='meetup_data', user='admin')
+    cur = con.cursor()
+    cur.execute('SELECT version()')
+    ver = cur.fetchone()
+    print ver
+
+except psycopg2.DatabaseError, e:
+
+    print 'Error %s' % e
+    sys.exit(1)
+
+finally:
+
+    if con:
+        con.close()
