09c0da3bb1301b055c2d406fa2d11a39ed51c8c7
Christian Lehner <lehner.chri@gmail.com>
2015-11-26 14:08:12 +0100

    changed relation names

--- neo4j_import.sh
+++ neo4j_import.sh
@@ -24,9 +24,9 @@ neo4j-import --into meetup.db --multiline-fields true --id-type Integer \
     --nodes:MEMBERS data/export/members.csv \
     --nodes:EVENTS data/export/events.csv \
     --nodes:VENUES data/export/venues.csv \
-    --relationships:HAVE data/export/groups_topics.csv \
-    --relationships:HAVE data/export/groups_categories.csv \
-    --relationships:INTERESTED data/export/members_topics.csv \
+    --relationships:IS_ABOUT data/export/groups_topics.csv \
+    --relationships:IN_CATEGORY data/export/groups_categories.csv \
+    --relationships:INTERESTED_IN data/export/members_topics.csv \
     --relationships:LOCATED_AT data/export/events_venues.csv \
     --relationships:RSVP data/export/events_members.csv \
     --relationships:HOSTED_BY data/export/events_groups.csv
